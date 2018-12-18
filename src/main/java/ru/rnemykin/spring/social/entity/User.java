6815583fb8e63e38223c2e5abedf57ba54646070
rnemykin <nemykin.roman@gmail.com>
2018-12-18 18:01:51 +0300

    added liquibase

--- src/main/java/ru/rnemykin/spring/social/entity/User.java
+++ src/main/java/ru/rnemykin/spring/social/entity/User.java
@@ -8,9 +8,11 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
 @Data
 @Entity
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
