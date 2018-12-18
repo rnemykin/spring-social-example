6842ea9428df1418e7b2f39307b01295a26e80b1
rnemykin <nemykin.roman@gmail.com>
2018-12-18 16:19:29 +0300

    init

---
+++ src/main/java/ru/rnemykin/spring/social/SpringSocialAuthApplication.java
@@ -0,0 +1,14 @@
+package ru.rnemykin.spring.social;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSocialAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSocialAuthApplication.class, args);
+    }
+
+}
+
